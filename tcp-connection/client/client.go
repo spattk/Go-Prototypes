ab7a8e4ef0e0a234219d50e390a6f31c4913390e
Sitesh Pattanaik <siteshpattanaik001@gmail.com>
2024-10-26 11:17:33 -0700

    tcp client server communication

---
+++ tcp-connection/client/client.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "localhost:8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	_, err = conn.Write([]byte("Hello World"))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	conn.Close()
+
+}
---
+++ tcp-connection/server/server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+    "fmt"
+    "net"
+)
+
+func main() {
+	//Setting up a tcp listener at port 8080
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//Accepting connections
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		// handle connection
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buf := make([]byte, 1024)
+	nb, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("Read %d bytes with text %s", nb, buf)
+}
