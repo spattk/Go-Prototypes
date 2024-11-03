b068a7b7e5d7950ef4863c7c73da0e9d5a3eaa43
Sitesh Pattanaik <siteshpattanaik001@gmail.com>
2024-11-03 09:07:35 -0800

    Insert benchmarking

--- mysql-index-benchmarking/main.go
+++ mysql-index-benchmarking/main.go
@@ -4,29 +4,22 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
-	"time"
 	"os"
+	"strings"
+	"strconv"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	if err := godotenv.Load(); err != nil {
-        log.Println("No .env file found")
-    }
-    dsn := os.Getenv("MYSQL_DSN")
-	db, err := sql.Open("mysql", dsn)
-	if err != nil {
-		fmt.Printf("Connection failed with %v", err)
-	} 
-	if err := db.Ping(); err != nil {
-        log.Fatalf("Error connecting to database: %v\n", err)
-    }
-    fmt.Println("Successfully connected to MySQL!")
+var (
+	count = 1000000
+)
 
-	// Example of running a query
-    rows, err := db.Query("SELECT * FROM mydate")
+func readFromDateTable(db *sql.DB) {
+	//query
+	rows, err := db.Query("SELECT * FROM mydate")
     if err != nil {
         log.Fatalf("Query error: %v\n", err)
     }
@@ -41,7 +34,58 @@ func main() {
         }
         fmt.Printf("\nPRINTING dt: %v, ts: %v\n", dt, ts)
     }
-	
+}
+
+func insertIntoTable(db *sql.DB, table string) (time.Duration, error) {
+    start := time.Now()
+	query := fmt.Sprintf("INSERT INTO %s (ID, Age) VALUES (?, ?)", table)
+	stmt, err := db.Prepare(query)
+	if err != nil {
+		log.Fatal(err)
+		return time.Since(start), err
+	}
+
+    for i := 1; i <= count; i++ {
+		if strings.Contains(table, "1") {
+			_, err = stmt.Exec(i+1, i+1)
+		} else {
+			_, err = stmt.Exec(strconv.Itoa(i+1), i+1)
+		}
+		
+        if err != nil {
+            log.Fatal(err)
+            return time.Since(start), err
+        }
+    }
+	stmt.Close()
+    return time.Since(start), nil
+}
+
+func main() {
+	if err := godotenv.Load(); err != nil {
+        log.Println("No .env file found")
+    }
+    dsn := os.Getenv("MYSQL_DSN")
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		fmt.Printf("Connection failed with %v", err)
+	} 
+	if err := db.Ping(); err != nil {
+        log.Fatalf("Error connecting to database: %v\n", err)
+    }
+    fmt.Println("Successfully connected to MySQL!")
 	defer db.Close()
+
+	// readFromDateTable(db)
+
+	time1, err1 := insertIntoTable(db, "test_table1")
+	if err1 == nil {
+		fmt.Printf("Inserted %v records in %v\n", count, time1)
+	}
+
+	time2, err2 := insertIntoTable(db, "test_table2")
+	if err2 == nil {
+		fmt.Printf("Inserted %v records in %v\n", count, time2)
+	}
 }
 	
